bb9b3a5569aa1e4cd99d1925ba2f3bf001c4ce73
Dawid Merchwa <merchwa.dawid@gmail.com>
2024-04-22 13:08:02 +0200

    [refactor]: Refactored authentication forms, added animated components and types
    In this commit, the following changes were made:
    
    *** MODIFIED ***
    - `utils/utils.ts`: Added a new function to handle when the user changes the authentication form.
    - `stores/authStore.ts`: Updated to utilize the new types from `types/auth.ts`.
    - `views/AuthView.vue`: The typewriter effect was moved to a new component called `AnimatedTypewriter.vue`.
    - All components from the `custom/form` directory were relocated to `custom/form/auth` directory and renamed to `Form{ComponentName}.vue`. They now also utilize the new `BaseFormAuth.vue` component.
    - All components from the `custom/svg` directory were moved to `custom/animated directory` and renamed to `Animated{ComponentName}.vue`.
    
    *** ADDED ***
    - `types/auth.ts`: Added types for the authentication forms.
    - `components/custom/form/auth/BaseFormAuth.vue`: Base component for the authentication forms.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,7 @@
 import { useColorMode } from '@vueuse/core';
+import { useAuthStore } from '@/stores/authStore';
+import { storeToRefs } from 'pinia';
+import type { AuthFormType } from '@/types/auth';
 
 export const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -6,4 +9,11 @@ export const toggleColorMode = () => {
   const mode = useColorMode();
 
   mode.value = mode.value === 'dark' ? 'light' : 'dark';
+};
+
+export const handleAuthFormTypeChange = (newAuthFormType: AuthFormType) => {
+  const authStore = useAuthStore();
+  const { authFormType } = storeToRefs(authStore);
+
+  authFormType.value = newAuthFormType;
 };
--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,67 +1,22 @@
 <script setup lang="ts">
-import LoginForm from '@/components/custom/form/LoginForm.vue';
-import RegisterForm from '@/components/custom/form/RegisterForm.vue';
-import ResetPasswordForm from '@/components/custom/form/ResetPasswordForm.vue';
-import WaveDividerSvg from '@/components/custom/svg/WaveDividerSvg.vue';
+import FormLogin from '@/components/custom/form/auth/FormLogin.vue';
+import FormRegister from '@/components/custom/form/auth/FormRegister.vue';
+import FormResetPassword from '@/components/custom/form/auth/FormResetPassword.vue';
+import AnimatedWave from '@/components/custom/animated/AnimatedWave.vue';
+import AnimatedTypewriter from '@/components/custom/animated/AnimatedTypewriter.vue';
+
+import { Icon } from '@iconify/vue';
+import { Button } from '@/components/ui/button';
 
 import { useColorMode } from '@vueuse/core';
-import { Button } from '@/components/ui/button';
-import { Icon } from '@iconify/vue';
-import { delay, toggleColorMode } from '@/utils/utils';
-import typewriterData from '@/assets/typewriterData.json';
+import { toggleColorMode } from '@/utils/utils';
 
-import { onMounted, ref } from 'vue';
 import { useAuthStore } from '@/stores/authStore';
 import { storeToRefs } from 'pinia';
 
 const mode = useColorMode();
 const authStore = useAuthStore();
-
-const typewriterTitleArray = [...typewriterData.title];
-const typewriterTextArray = [...typewriterData.text];
-
 const { authFormType } = storeToRefs(authStore);
-const fadeOut = ref(false);
-const fadeIn = ref(false);
-const typewriterTitle = ref('');
-const typewriterText = ref('');
-
-// Typewriter effect
-const typeText = async () => {
-  // Get a random index from the array
-  let i = Math.floor(Math.random() * typewriterTitleArray.length);
-
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    fadeOut.value = false;
-    fadeIn.value = true;
-    typewriterTitle.value = typewriterTitleArray[i];
-    typewriterText.value = '';
-
-    for (const char of typewriterTextArray[i]) {
-      typewriterText.value += char;
-
-      // Random delay between typing each character, it will look more natural
-      await delay(Math.random() * 50 + 20);
-    }
-
-    await delay(3500);
-    fadeOut.value = true;
-    fadeIn.value = false;
-    await delay(700);
-
-    i++;
-
-    // Reset the index if it's equal to the length of the array
-    if (i === typewriterTextArray.length) {
-      i = 0;
-    }
-  }
-};
-
-onMounted(async () => {
-  await typeText();
-});
 </script>
 
 <template>
@@ -69,24 +24,7 @@ onMounted(async () => {
     <div class="p-2 grid grid-rows-auto-1fr">
       <p>!!! LOGO PLACEHOLDER !!!</p>
       <div class="self-center w-5/6 ml-10">
-        <h1
-          class="text-3xl font-semibold tracking-tighter"
-          :class="{
-            'fade-out-base fade-out-slower': fadeOut,
-            'fade-in-base fade-in-faster': fadeIn
-          }"
-        >
-          {{ typewriterTitle }}
-        </h1>
-        <p
-          class="h-20 text-2xl tracking-tight"
-          :class="{
-            'fade-out-base fade-out-faster': fadeOut,
-            'fade-in-base fade-in-slower': fadeIn
-          }"
-        >
-          {{ typewriterText }}<span class="cursor">|</span>
-        </p>
+        <AnimatedTypewriter />
       </div>
     </div>
     <div class="grid grid-rows-auto-1fr">
@@ -105,62 +43,10 @@ onMounted(async () => {
           />
         </Button>
       </div>
-      <LoginForm v-if="authFormType === 'login'" />
-      <RegisterForm v-else-if="authFormType === 'register'" />
-      <ResetPasswordForm v-else />
+      <FormLogin v-if="authFormType === 'login'" />
+      <FormRegister v-else-if="authFormType === 'register'" />
+      <FormResetPassword v-else />
     </div>
-    <WaveDividerSvg />
+    <AnimatedWave />
   </div>
 </template>
-
-<style scoped>
-.cursor {
-  animation: blink 1s infinite;
-}
-
-.fade-in-base {
-  opacity: 1;
-  transform: translateY(-20px);
-}
-
-.fade-in-faster {
-  transition:
-    opacity 0.25s ease,
-    transform 0.25s ease;
-}
-
-.fade-in-slower {
-  transition:
-    opacity 0.6s ease,
-    transform 0.6s ease;
-}
-
-.fade-out-base {
-  opacity: 0;
-  transform: translateY(20px);
-}
-
-.fade-out-faster {
-  transition:
-    opacity 0.25s ease,
-    transform 0.25s ease;
-}
-
-.fade-out-slower {
-  transition:
-    opacity 0.6s ease,
-    transform 0.6s ease;
-}
-
-@keyframes blink {
-  0% {
-    opacity: 1;
-  }
-  50% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-</style>
