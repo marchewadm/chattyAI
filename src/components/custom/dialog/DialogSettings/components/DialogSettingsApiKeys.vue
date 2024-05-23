27940227f814b55059b6f52c225776cf572803b1
marchewadm <merchwa.dawid@gmail.com>
2024-05-23 19:30:19 +0200

    [feat]: create form for dynamic api keys

--- src/components/custom/dialog/DialogSettings/components/DialogSettingsApiKeys.vue
+++ src/components/custom/dialog/DialogSettings/components/DialogSettingsApiKeys.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+// --- IMPORTS ---
 import ButtonIcon from '@/components/custom/button/ButtonIcon.vue';
-
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useToast } from '@/components/ui/toast';
 import { ScrollArea } from '@/components/ui/scroll-area';
+import { FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import {
@@ -16,60 +17,73 @@ import {
   CommandList
 } from '@/components/ui/command';
 
-import { createReusableTemplate, useMediaQuery } from '@vueuse/core';
 import { ref } from 'vue';
+import { toRawDeep } from '@/utils/utils';
 
-interface Status {
+import { createReusableTemplate, useMediaQuery } from '@vueuse/core';
+
+import * as z from 'zod';
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+// --------------------------------------------------------------
+
+// --- INTERFACES ---
+interface AiModel {
   value: string;
   label: string;
 }
 
 interface ApiKey {
   id: number;
-  apiKey: string | undefined;
-  status: Status | null;
+  key: string | undefined;
+  aiModel: AiModel | null;
   isOpen: boolean;
 }
+// --------------------------------------------------------------
 
-const { toast } = useToast();
-const [DefineTemplate, ReuseTemplate] = createReusableTemplate();
-const isDesktop = useMediaQuery('(min-width: 768px');
-
-const statuses: Status[] = [
+// --- CONSTANTS ---
+const aiModels: AiModel[] = [
   {
     value: 'gpt-3.5',
     label: 'GPT-3.5'
-  },
-  {
-    value: 'gpt-4',
-    label: 'GPT-4'
   }
 ];
-
 const apiKeys = ref<ApiKey[]>([
   {
     id: 0,
-    apiKey: undefined,
-    status: null,
+    key: undefined,
+    aiModel: null,
     isOpen: false
   }
 ]);
 
-const onStatusSelect = (status: Status, id: number) => {
+const { toast } = useToast();
+const [DefineTemplate, ReuseTemplate] = createReusableTemplate();
+const isDesktop = useMediaQuery('(min-width: 768px)');
+// --------------------------------------------------------------
+
+// --- FUNCTIONS ---
+const onAiModelSelect = (aiModel: AiModel, id: number) => {
   const index = apiKeys.value.findIndex((apiKey) => apiKey.id === id);
 
-  if (index !== -1) {
-    apiKeys.value[index].status = status;
-    apiKeys.value[index].isOpen = false;
-  }
+  if (!(index !== -1)) return;
+
+  apiKeys.value[index].aiModel = aiModel;
+  apiKeys.value[index].isOpen = false;
 };
 
-const addApiKey = (apiKey: string | undefined, status: Status | null) => {
-  if (apiKey && status) {
+const addApiKey = (apiKey: ApiKey) => {
+  if (apiKey.key && apiKey.aiModel) {
+    // If new API key was successfully added, create a new empty input field
+    setFieldValue(`apiKeys.${apiKey.id}`, {
+      apiKey: apiKey.key,
+      aiModel: apiKey.aiModel.value
+    });
+
     apiKeys.value.push({
       id: apiKeys.value.length,
-      apiKey: undefined,
-      status: null,
+      key: undefined,
+      aiModel: null,
       isOpen: false
     });
   } else {
@@ -84,85 +98,140 @@ const addApiKey = (apiKey: string | undefined, status: Status | null) => {
 const removeApiKey = (id: number) => {
   const index = apiKeys.value.findIndex((apiKey) => apiKey.id === id);
 
-  if (index !== -1) {
-    apiKeys.value.splice(index, 1);
-  }
+  if (index === -1) return;
+
+  apiKeys.value.splice(index, 1);
+
+  const rawValues = structuredClone(toRawDeep(values));
+  rawValues.apiKeys?.splice(index, 1);
+
+  setValues(rawValues);
 };
+
+// --- FORM ---
+const formSchema = toTypedSchema(
+  z.object({
+    apiKeys: z
+      .array(
+        z.object({
+          apiKey: z.string().min(1).optional(),
+          aiModel: z.string().optional()
+        })
+      )
+      .refine(
+        (apiKeys) => {
+          return apiKeys.every((apiKey) => {
+            return !apiKey.apiKey || (apiKey.apiKey && apiKey.aiModel);
+          });
+        },
+        {
+          message: 'Please provide a valid API key and select a model',
+          path: ['apiKeys']
+        }
+      )
+  })
+);
+const { handleSubmit, setFieldValue, setValues, values } = useForm({
+  validationSchema: formSchema
+});
+const onSubmit = handleSubmit((values) => {
+  const validApiKeys = values.apiKeys.filter((apiKey) => apiKey.apiKey && apiKey.aiModel);
+
+  console.log(validApiKeys);
+});
 </script>
 
 <template>
   <ScrollArea class="h-full">
-    <div class="flex flex-col gap-2 mb-4 py-1 pl-1 pr-3">
-      <div
-        class="grid api-keys-layout gap-2 items-center"
-        v-for="apiKey in apiKeys"
-        :key="apiKey.id"
-      >
-        <Input type="text" placeholder="Your API Key" v-model="apiKey.apiKey" required />
-        <div>
-          <DefineTemplate>
-            <Command>
-              <CommandInput placeholder="Filter models..." />
-              <CommandList>
-                <CommandEmpty>No results found.</CommandEmpty>
-                <CommandGroup>
-                  <CommandItem
-                    v-for="status of statuses"
-                    :key="status.value"
-                    :value="status.value"
-                    @select="onStatusSelect(status, apiKey.id)"
-                  >
-                    {{ status.label }}
-                  </CommandItem>
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          </DefineTemplate>
+    <form @submit="onSubmit">
+      <div class="flex flex-col gap-2 mb-4 py-1 pl-1 pr-3">
+        <div
+          class="grid api-keys-layout gap-2 items-center"
+          v-for="(apiKey, index) in apiKeys"
+          :key="apiKey.id"
+        >
+          <FormField :name="`apiKeys.${index}.apiKey`">
+            <FormItem>
+              <FormControl>
+                <Input type="text" placeholder="Your API Key" v-model="apiKey.key" />
+              </FormControl>
+            </FormItem>
+          </FormField>
+          <div>
+            <DefineTemplate>
+              <Command>
+                <CommandInput placeholder="Filter models..." />
+                <CommandList>
+                  <CommandEmpty>No results found.</CommandEmpty>
+                  <CommandGroup>
+                    <CommandItem
+                      v-for="aiModel in aiModels"
+                      :key="aiModel.value"
+                      :value="aiModel.value"
+                      @select="onAiModelSelect(aiModel, apiKey.id)"
+                    >
+                      {{ aiModel.label }}
+                    </CommandItem>
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </DefineTemplate>
 
-          <Popover v-if="isDesktop" v-model:open="apiKey.isOpen">
-            <PopoverTrigger as-child>
-              <Button variant="outline" class="w-[150px] justify-start">
-                {{ apiKey.status ? apiKey.status.label : '+ Select model' }}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent class="w-[200px] p-0" align="start">
-              <ReuseTemplate />
-            </PopoverContent>
-          </Popover>
+            <FormField name="aiModel">
+              <FormItem>
+                <Popover v-if="isDesktop" v-model:open="apiKey.isOpen">
+                  <PopoverTrigger as-child>
+                    <FormControl>
+                      <Button variant="outline" class="w-[150px] justify-start">
+                        {{ apiKey.aiModel ? apiKey.aiModel.label : '+ Select model' }}
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent class="w-[200px] p-0" align="start">
+                    <ReuseTemplate />
+                  </PopoverContent>
+                </Popover>
 
-          <Drawer v-else>
-            <DrawerTrigger as-child>
-              <Button variant="outline" class="w-[150px] justify-start">
-                {{ apiKey.status ? apiKey.status.label : '+ Select model' }}
-              </Button>
-            </DrawerTrigger>
-            <DrawerContent>
-              <div class="mt-4 border-t">
-                <ReuseTemplate />
-              </div>
-            </DrawerContent>
-          </Drawer>
+                <Drawer v-else>
+                  <DrawerTrigger as-child>
+                    <FormControl>
+                      <Button variant="outline" class="w-[150px] justify-start">
+                        {{ apiKey.aiModel ? apiKey.aiModel.label : '+ Select model' }}
+                      </Button>
+                    </FormControl>
+                  </DrawerTrigger>
+                  <DrawerContent>
+                    <div class="mt-4 border-t">
+                      <ReuseTemplate />
+                    </div>
+                  </DrawerContent>
+                </Drawer>
+              </FormItem>
+            </FormField>
+          </div>
+          <ButtonIcon
+            type="button"
+            iconName="add-outline"
+            class="w-8 h-8"
+            size="icon"
+            v-show="apiKey === apiKeys[apiKeys.length - 1]"
+            @click="addApiKey(apiKey)"
+          />
+          <ButtonIcon
+            type="button"
+            iconName="trash-outline"
+            class="w-8 h-8"
+            size="icon"
+            variant="destructive"
+            v-show="apiKey !== apiKeys[apiKeys.length - 1]"
+            @click="removeApiKey(apiKey.id)"
+          />
         </div>
-        <ButtonIcon
-          iconName="add-outline"
-          class="w-8 h-8"
-          size="icon"
-          v-show="apiKey === apiKeys[apiKeys.length - 1]"
-          @click="addApiKey(apiKey.apiKey, apiKey.status)"
-        />
-        <ButtonIcon
-          iconName="trash-outline"
-          class="w-8 h-8"
-          size="icon"
-          variant="destructive"
-          v-show="apiKey !== apiKeys[apiKeys.length - 1]"
-          @click="removeApiKey(apiKey.id)"
-        />
       </div>
-    </div>
-    <div class="flex justify-end pr-3">
-      <Button>Save changes</Button>
-    </div>
+      <div class="flex justify-end pr-3">
+        <Button>Save changes</Button>
+      </div>
+    </form>
   </ScrollArea>
 </template>
 
