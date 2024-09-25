7aeeefe85ee4363559de800b8f058c515131ddcb
marchewadm <merchwa.dawid@gmail.com>
2024-09-25 21:26:53 +0200

    feat: set focus on textarea element

--- src/components/custom/chat/Textarea.vue
+++ src/components/custom/chat/Textarea.vue
@@ -2,20 +2,31 @@
 import { Textarea } from '@/components/ui/textarea';
 import ButtonIconChatAction from '@/components/custom/button/ButtonIconChatAction.vue';
 
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import { onBeforeRouteUpdate } from 'vue-router';
+import { useFocus } from '@vueuse/core';
 
 const message = ref('');
+const textareaEl = ref<HTMLTextAreaElement | null>(null);
 
 const sendMessage = () => {
-  if (!message.value) {
-    return;
-  }
+  if (!message.value) return;
 
   emit('sendMessage', message.value);
   message.value = '';
 };
 
 const emit = defineEmits(['sendMessage']);
+
+const useFocusOnTextarea = () => useFocus(textareaEl, { initialValue: true });
+
+onMounted(() => {
+  useFocusOnTextarea();
+});
+
+onBeforeRouteUpdate(() => {
+  useFocusOnTextarea();
+});
 </script>
 
 <template>
@@ -24,6 +35,7 @@ const emit = defineEmits(['sendMessage']);
       placeholder="Type your message here..."
       class="dark:border-gray-700 h-10 p-4 resize-none rounded-2xl"
       v-model="message"
+      ref="textareaEl"
       @keydown.enter.prevent="sendMessage"
     />
     <div class="flex absolute left-full top-1/2 -translate-y-1/2">
